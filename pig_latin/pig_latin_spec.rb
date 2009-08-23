4e6fac2d692a81bf8b3eeb9834cc72a43b7d867a
Sarah Allen <sarah@ultrasaurus.com>
2009-08-23 13:57:10 -0700

    added shoulda and rspec for comparison

---
+++ pig_latin/pig_latin_spec.rb
@@ -0,0 +1,34 @@
+require 'pig_latin'
+
+describe "#translate" do
+  include PigLatinTranslator
+
+  it "should translate a simple word: nix" do
+    s = translate("nix")
+    s.should == "ixnay"
+  end
+
+  it "should translate a word beginning with a vowel: apple" do
+    pending
+    s = translate("apple")
+    s.should == "appleay"
+  end
+
+  it "should translate a two consonent word: stupid" do
+    pending
+    s = translate("stupid")
+    s.should == "upidstay"
+  end
+
+  it "should translate two words: eat pie" do
+    pending
+    s = translate("eat pie")
+    s.should == "eatay iepay"
+  end
+
+  it "should translate multiple words: the quick brown fox" do
+    pending
+    s = translate("the quick brown fox")
+    s.should == "ethay ickquay ownbray oxfay"
+  end
+end
