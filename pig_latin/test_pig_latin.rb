30a0a23d708eb9e308d9617a821f87e98c60cce9
Sarah Allen <sarah@ultrasaurus.com>
2009-08-23 14:03:30 -0700

    merged test_pig_latin.rb

--- pig_latin/test_pig_latin.rb
+++ pig_latin/test_pig_latin.rb
@@ -4,30 +4,29 @@ require 'pig_latin'
 class PigLatinTest < Test::Unit::TestCase
     include PigLatinTranslator
 
-    def test_simple_word
+    def test_simple_word_nix
         s = translate("nix")
-        assert_equal("ixnay", s, "'nix' translation failed")
+        assert_equal("ixnay", s)
     end
 
-    def test_word_beginning_with_vowel
+    def test_word_beginning_with_vowel_apple
         s = translate("apple")
-        assert_equal("appleay", s, "'apple' translation failed")
+        assert_equal("appleay", s)
     end
 
-    def test_two_consonant_word
+    def test_two_consonant_word_stupid
         s = translate("stupid")
-        assert_equal("upidstay", s, "'stupid' translation failed")
+        assert_equal("upidstay", s)
     end
 
-    def test_two_words
+    def test_two_words_eat_pie
         s = translate("eat pie")
-        assert_equal("eatay iepay", s, "'eat pie' translation failed")
+        assert_equal("eatay iepay", s)
     end
 
-    def test_multiple_words
+    def test_multiple_words_the_quick_brown_fox
         s = translate("the quick brown fox")
-        assert_equal("ethay ickquay ownbray oxfay", s,
-            "'the quick brown fox' translation failed")
+        assert_equal("ethay ickquay ownbray oxfay", s)
     end
 
 end
