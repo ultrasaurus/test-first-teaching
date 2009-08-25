aa4e94777caa0c20da6bdfc06a091e4e471397f7
Alex Chaffee <alex@stinky.com>
2009-08-25 10:08:45 -0700

    trillions is now 'pending'

--- in_words/in_words_spec.rb
+++ in_words/in_words_spec.rb
@@ -76,9 +76,11 @@ describe Fixnum do
   end
   
   it "reads trillions" do
-    1000000000000.in_words.should == 'one trillion'
-    1000000000001.in_words.should == 'one trillion one'
-    1888259040036.in_words.should == 'one trillion eight hundred eighty eight billion two hundred fifty nine million forty thousand thirty six'
+    pending do
+      1000000000000.in_words.should == 'one trillion'
+      1000000000001.in_words.should == 'one trillion one'
+      1888259040036.in_words.should == 'one trillion eight hundred eighty eight billion two hundred fifty nine million forty thousand thirty six'
+    end
   end
   
 end
