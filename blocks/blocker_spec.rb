0b36085cc587e324ac72c9377a4b3f8ae60c8b66
Sarah Allen <sarah@ultrasaurus.com>
2009-08-30 19:52:22 -0700

    new simpler version of blocker

--- blocks/blocker_spec.rb
+++ blocks/blocker_spec.rb
@@ -1,27 +1,46 @@
 require "blocker"
 
 describe Blocker do
-  it "executes a block and returns the value" do
-    Blocker.execute do
-      5
-    end.should == 5
+
+  it "executes a block and returns a string" do
+    result = Blocker.new.execute do
+      "hello"
+    end
+    result.should == "hello"
   end
 
-  it "works with braces too" do
-    Blocker.execute { 5 }.should == 5
+  it "executes a block that returns a number" do
+    pending
+    result = Blocker.new.execute do
+      3 + 4
+    end
+    result.should == 7
+  end
+
+  it "executes a block in context" do
+    pending
+    n = nil
+    Blocker.new.execute do
+      n = 7
+    end
+    n.should == 7
+  end
+
+  it "executes a block 3 times" do
+    pending
+    n = 0
+    Blocker.new.execute(3) do
+      n += 1
+    end
+    n.should == 3
+
   end
 
   it "adds one to the value returned by the block" do
+    pending
     Blocker.add_one do
       5
     end.should == 6
   end
 
-  it "passes a value in to the block" do
-    x = nil
-    Blocker.receive(3) do |value|
-      x = value
-    end
-    x.should == 3
-  end
 end
