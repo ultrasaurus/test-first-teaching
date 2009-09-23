c1817287ba6c28d545d41a280e1b2e5bb431d325
Sarah Allen <sarah@ultrasaurus.com>
2009-09-22 19:04:17 -0700

    controller specs

---
+++ roster/spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe MessagesController do
+
+    describe "GET hello" do
+
+      it "renders hello" do
+        get :hello
+        response.should have_text(/Hello/)
+      end
+
+      it "renders hello with name" do
+        params[:name] = "Sarah"
+        get :hello, params
+        response.should have_text("Hello Sarah")
+      end
+
+    end
+    
+    describe "GET fancy_hello" do
+
+      it "renders fancy_hello" do
+        get :fancy_hello
+        response.should render_template('fancy_hello.html')
+      end
+
+      it "renders fancy_hello with name" do
+        name = "Mike"
+        params[:name] = name
+
+        get :fancy_hello, params
+        assigns[:name].should == name
+        response.should render_template('fancy_hello.html')
+      end
+    end
+end
