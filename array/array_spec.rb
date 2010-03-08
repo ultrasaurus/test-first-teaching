32973ca73e9d6018f14412644e5f2780ee62552d
Kai Middleton <kai.middleton@gmail.com>
2010-03-07 17:18:48 -0800

    fixed silly omission so that the total would work

--- array/array_spec.rb
+++ array/array_spec.rb
@@ -3,13 +3,13 @@ require "array_extension"
 describe Array do
   before do
     @my_array1 = [1,2,4]
-    @my_array2 = [1,3,5,9,11]
+    @my_array2 = [1,3,5,7,9,11]
   end
 
   describe "#sum" do
     it "should add all of the elements" do
       @my_array1.sum.should equal 7
-      @my_array2.sum.should equal 25
+      @my_array2.sum.should equal 36
     end
   
   end
