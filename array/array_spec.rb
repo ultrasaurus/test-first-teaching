dc8c4d57156741e7aa852ddb01bb52ec35f1e1db
Kai Middleton <kai.middleton@gmail.com>
2010-03-07 16:43:40 -0800

    Added a second array to insure that a simplistic implementation won't pass spec.

--- array/array_spec.rb
+++ array/array_spec.rb
@@ -2,13 +2,14 @@ require "array_extension"
 
 describe Array do
   before do
-    @my_array = [1,2,4]
+    @my_array1 = [1,2,4]
+    @my_array2 = [1,3,5,9,11]
   end
 
   describe "#sum" do
     it "should add all of the elements" do
-      @my_array.sum.should equal 7
-
+      @my_array1.sum.should equal 7
+      @my_array2.sum.should equal 25
     end
   
   end
