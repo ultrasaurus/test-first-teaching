5ab0640ef939fd0dbf01a35ab5f2665d397d1f2b
Sarah Allen <sarah@ultrasaurus.com>
2010-03-14 12:51:25 -0700

    changed simon_says so it doesn't require a class

--- learn_ruby/simon_says/simon_spec.rb
+++ learn_ruby/simon_says/simon_spec.rb
@@ -1,28 +1,42 @@
-require 'simon'
-
-describe Simon do
+describe "Simon says" do
   
-  it "can echo hello" do
-    Simon.new.echo("hello").should == "hello"
+  it "should echo hello" do
+    echo("hello").should == "hello"
   end
   
-  it "can echo bye" do
-    Simon.new.echo("bye").should == "bye"
+  it "should echo bye" do
+    echo("bye").should == "bye"
   end
 
-  it "can shout hello" do
-    Simon.new.shout("hello").should == "HELLO"
+  it "should shout hello" do
+    shout("hello").should == "HELLO"
   end
   
-  it "can shout multiple words" do
-    Simon.new.shout("hello world").should == "HELLO WORLD"
+  it "should shout multiple words" do
+    shout("hello world").should == "HELLO WORLD"
   end
 
-  it "can repeat" do
-    Simon.new.repeat("hello").should == "hello hello"
+  it "should repeat" do
+    repeat("hello").should == "hello hello"
   end
 
-  it "can repeat a number of times" do
-    Simon.new.repeat("hello", 3).should == "hello hello hello"
+  it "should repeat a number of times" do
+    repeat("hello", 3).should == "hello hello hello"
+  end
+
+  it "should return the first letter" do
+    start_of_word("hello", 1).should == "h"
+  end
+  
+  it "should return the first two letters" do
+    start_of_word("Bob", 2).should == "Bo"
+  end
+
+  it "should tell us the first word of 'Hello World' is 'Hello'" do
+    first_word("Hello World").should == "Hello"
+  end
+
+  it "should tell us the first word of 'oh dear' is 'oh'" do
+    first_word("Hello World").should == "oh"
   end
 end
