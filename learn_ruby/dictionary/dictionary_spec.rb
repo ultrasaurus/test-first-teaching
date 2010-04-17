bd9b4dfbcb0fa38f11dbce484406d5d425181760
Kai Middleton <kai.middleton@gmail.com>
2010-04-17 00:08:36 -0700

    refactored dictionary spec and added some more tests

--- learn_ruby/dictionary/dictionary_spec.rb
+++ learn_ruby/dictionary/dictionary_spec.rb
@@ -1,54 +1,66 @@
 require 'dictionary'
 
 describe Dictionary do
+  before do
+    @d = Dictionary.new
+  end
+  
   it "should be empty when created" do
-    d = Dictionary.new
-    d.words == []
+    @d.words == []
   end
 
   it "should report its contents" do 
-    d = Dictionary.new
-    d.add("fish")
-    d.add("foul")
-    d.words.sort == ["fish", "foul"].sort
+    @d.add("fish")
+    @d.add("foul")
+    @d.words.sort == ["fish", "foul"].sort
   end
 
   it "should not include a word in an empty dictionary" do
-    d = Dictionary.new
-    d.include?('fish').should be_false
+    @d.include?('fish').should be_false
   end
 
   it "should not include a prefix that wasn't added as a word in and of itself" do
-    d = Dictionary.new
-    d.add('fish')
-    d.include?('fi').should be_false
+    @d.add('fish')
+    @d.include?('fi').should be_false
   end
 
   it "should be able to add words" do
-    d = Dictionary.new
-    d.add('fish')
-    d.include?('fish').should be_true
+    @d.add('fish')
+    @d.include?('fish').should be_true
   end
 
   it "should not find word in empty dictionary" do
-    d = Dictionary.new
-    d.find('fi').should == []
+    @d.find('fi').should == []
+  end
+
+  it "should find nothing if the prefix matches nothing" do
+    @d.add('fiend')
+    @d.add('great')
+    @d.find('nothing').should == []
   end
 
   it "should find a word from a prefix" do
-    d = Dictionary.new
-    d.add('fish')
-    d.add('fiend')
-    d.add('great')
-    d.find('gr').should == ['great']
+    @d.add('fish')
+    @d.add('fiend')
+    @d.add('great')
+    @d.find('gr').should == ['great']
   end
 
   it "should find multiple matches from a prefix" do
-    d = Dictionary.new
-    d.add('fish')
-    d.add('fiend')
-    d.add('great')
-    d.find('fi').sort.should == ['fish', 'fiend'].sort
+    @d.add('fish')
+    @d.add('fiend')
+    @d.add('great')
+    @d.find('fi').sort.should == ['fish', 'fiend'].sort
   end
+  
+  it "should find correct matches and disregard partial matches" do
+    @d.add('fan')
+    @d.add('fish')
+    @d.add('fiend')
+    @d.add('great')
+    @d.find('fi').sort.should == ['fish', 'fiend'].sort
+  end
+  
+  
 end
 
