54bcb221b7bb66848aa9644d4d6975a83d0ef8bb
Alex Chaffee <alex@stinky.com>
2011-05-17 12:10:34 -0700

    google analytics

--- web/page.rb
+++ web/page.rb
@@ -33,6 +33,22 @@ class Page < Erector::Widgets::Page
     style raw(Sass::Engine.for_file("#{here}/tft-web.scss", {}).render)
   end
   
+  def self.google_analytics_code account_id
+    <<-JAVASCRIPT
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '#{account_id}']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    JAVASCRIPT
+  end
+
+  external :script, google_analytics_code('UA-23415770-1')
+  
   def body_content
     div :class => "wrapper" do
       headline
