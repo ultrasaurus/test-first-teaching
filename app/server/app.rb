1aef5f4aa0f3db5fbb8a437169c2d46373d09743
Alex Chaffee <alex@stinky.com>
2011-09-28 15:46:14 -0700

    messing with

--- app/server/app.rb
+++ app/server/app.rb
@@ -1,8 +1,19 @@
 require 'sinatra'
 
 get "/" do
-  puts params[:cmd]
-  result = eval(params[:cmd])
-  puts result
-  result.to_s
+  if params[:cmd]
+    begin
+      $SAFE = 3
+      cmd = params[:cmd]
+      result = eval("$SAFE = 3;#{cmd}")
+      return "<pre>#{result}</pre>"
+    rescue Exception => e
+      p e
+      return "<pre>#{e.class}: #{e.message}</pre>"
+    ensure
+      $SAFE = 0
+    end
+  else
+    "<form><input name='cmd'></form>"
+  end
 end
