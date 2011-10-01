70d219e2fbabfff6a9d9019402b57b2ea22d3e61
Alex Chaffee <alex@stinky.com>
2011-09-30 18:45:53 -0700

    refactoring

--- learn_ruby/bottles/bottles.rb
+++ learn_ruby/bottles/bottles.rb
@@ -1,3 +1,7 @@
+def on_the_wall bottle_count
+  "#{bottles_of_beer(bottle_count)} on the wall."
+end
+
 def bottles_of_beer bottle_count
   if bottle_count == 1
     "1 bottle of beer"
@@ -6,22 +10,30 @@ def bottles_of_beer bottle_count
   end
 end
 
+def verse(bottle_count)
+  "#{on_the_wall(bottle_count)} #{bottles_of_beer(bottle_count)}."
+end
+
+def take_one_down
+  "Take one down and pass it around."
+end
+
 def bottles(bottle_count)
   out = ""
 
   while bottle_count > 1
     out += <<-MESSAGE
-#{bottles_of_beer(bottle_count)} on the wall. #{bottles_of_beer(bottle_count)}.
-Take one down and pass it around.
-#{bottles_of_beer(bottle_count - 1)} on the wall.
+#{verse(bottle_count)}
+#{take_one_down}
+#{on_the_wall(bottle_count - 1)}
     MESSAGE
     bottle_count -= 1
   end
 
   if bottle_count == 1
     out += <<-MESSAGE
-1 bottle of beer on the wall. 1 bottle of beer.
-Take one down and pass it around.
+#{verse(bottle_count)}
+#{take_one_down}
     MESSAGE
   end
 
